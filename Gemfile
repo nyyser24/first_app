eb2252c62ba0ad9cf34ac6ee400d30c998e9f011
serik <serik08@gmail.com>
2011-03-07 21:00:30 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+gem 'sqlite3-ruby', '1.2.5', :require => 'sqlite3'
+
+group :development do
+  gem 'annotate-models', '1.0.4' 
+  gem "paperclip", "~> 2.3"
+end
+
+group :test do
+  gem 'rspec', '2.3.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '601d2614229d508ae4b2efac31e3b629deb469a3263a2c3fa1d7f99a3c5a274a9f1d0e15a5c975fbdfdf46c831102b857a0d3c92e62bd14991c7cad123cb8bb5'
